f9d327b020e11a79d7660d6c74e2f3bc492ef76a
Carlos Pineda <carlospineda@nicahost.net>
2026-09-16 17:04:16 -0600

    Se agrego el primer ejercicio de OOP

---
+++ Python Intermedio/Ejercicios OOP/circle.py
@@ -0,0 +1,13 @@
+import math
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_area(self):
+        area = math.pi * self.radius**2
+        return area
+
+
+my_circle = Circle(5)
